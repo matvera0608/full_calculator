6cb60958c257547ba915e8f86031e47c251289c0
Ramiro <135643361+matvera0608@users.noreply.github.com>
2025-04-09 17:51:08 -0300

    Add files via upload

---
+++ Calculadora simple.py
@@ -0,0 +1,18 @@
+from tkinter import *
+import tkinter as tk, tkinter.messagebox as mensajeDeTexto, tkinter.font as fuenteDeLetra, tkinter.simpledialog as diálogo
+
+# -*- coding: utf-8 -*-
+def calculadora():
+    ventanaPrincipal = tk.Tk()
+    ventanaPrincipal.title("Calculadora sencilla")
+    ventanaPrincipal.geometry("720x1280")
+    ventanaPrincipal.config(bg="white")
+    ventanaPrincipal.resizable(10, 10)
+    
+    return ventanaPrincipal
+
+#Defino una variable para disparar la interfaz gráfica de calculadora
+
+calculadora_principal = calculadora()
+
+calculadora_principal.mainloop()
